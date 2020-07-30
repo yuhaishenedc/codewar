96269099c339f21885d05c6abbf45c142ec91da1
yuhaishen <2249094924@qq.com>
2020-07-30 12:35:33 +0800

    add 10.01

---
+++ code/leetcode/Sorted_Merge_LCCI_10_01.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class Sorted_Merge_LCCI_10_01 {
+
+    static class Solution {
+        public void merge(int[] A, int m, int[] B, int n) {
+            for(int i=m;i<m+n;i++){
+                A[i]=B[i-m];
+            }
+            Shell_sort(A);
+        }
+
+        public static void Shell_sort(int[] arr){
+            int len=arr.length;
+            int shell=len/2;
+            while(shell>0){
+                for(int i=shell;i<len;i++){
+                    if(arr[i]<arr[i-shell]){
+                        int temp=arr[i];
+                        for(int j=i;j>=0;j-=shell){
+                            if(j-shell>=0&&temp<arr[j-shell]){
+                                arr[j]=arr[j-shell];
+                            }
+                            else{
+                                arr[j]=temp;
+                                break;
+                            }
+                        }
+                    }
+                }
+                shell/=2;
+            }
+        }
+    }
+
+
+}
--- code/test.java
+++ code/test.java
@@ -1,8 +1,37 @@
 import java.util.*;
 public class test{
-    public static void main(String[] args) {
 
+}
+
+
+class Solution {
+    public void merge(int[] A, int m, int[] B, int n) {
+        for(int i=m;i<m+n;i++){
+            A[i]=B[i-m];
+        }
+        Shell_sort(A);
     }
 
+    public static void Shell_sort(int[] arr){
+        int len=arr.length;
+        int shell=len/2;
+        while(shell>0){
+            for(int i=shell;i<len;i++){
+                if(arr[i]<arr[i-shell]){
+                    int temp=arr[i];
+                    for(int j=i;j>=0;j-=shell){
+                        if(j-shell>=0&&temp<arr[j-shell]){
+                            arr[j]=arr[j-shell];
+                        }
+                        else{
+                            arr[j]=temp;
+                            break;
+                        }
+                    }
+                }
+            }
+            shell/=2;
+        }
+    }
 }
 
