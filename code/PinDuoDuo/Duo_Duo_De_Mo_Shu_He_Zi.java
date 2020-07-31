8070b99573fc9db44ffa789926087209a594d33e
yuhaishen <2249094924@qq.com>
2020-07-31 09:36:23 +0800

    add 88

---
+++ code/PinDuoDuo/Duo_Duo_De_Mo_Shu_He_Zi.java
@@ -0,0 +1,60 @@
+package PinDuoDuo;
+
+//多多鸡有N个魔术盒子（编号1～N），其中编号为i的盒子里有i个球
+//多多鸡让皮皮虾每次选择一个数字X（1 <= X <= N），多多鸡就会把球数量大于等于X个的盒子里的球减少X个。
+//通过观察，皮皮虾已经掌握了其中的奥秘，并且发现只要通过一定的操作顺序，可以用最少的次数将所有盒子里的球变没。
+//那么请问聪明的你，是否已经知道了应该如何操作呢？
+
+//输入描述:
+//第一行，有1个整数T，表示测试用例的组数。
+// （1 <= T <= 100）
+//接下来T行，每行1个整数N，表示有N个魔术盒子。
+//（1 <= N <= 1,000,000,000）
+
+//输出描述:
+//共T行，每行1个整数，表示要将所有盒子的球变没，最少需要进行多少次操作。
+
+//输入例子1：
+//3
+//1
+//2
+//5
+
+//输出例子1：
+//1
+//2
+//3
+
+
+import java.util.*;
+public class Duo_Duo_De_Mo_Shu_He_Zi {
+    //divide and conquer
+
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        for(int i=0;i<n;i++){
+            int[] arr=new int[n];
+            int max=0;
+            for(int j=0;j<n;j++){
+                arr[j]=input.nextInt();
+                if(arr[j]>max){
+                    max=arr[j];
+                }
+            }
+            int[] dp=new int[max+2];
+            int sum=0;
+            int count=0;
+            for(int j=1;j<=max;j++){
+                if(sum<j){
+                    sum+=j;
+                    count++;
+                }
+                dp[j]=count;
+            }
+            for(int j=0;j<n;j++){
+                System.out.println(dp[arr[j]]);
+            }
+        }
+    }
+}
--- code/test.java
+++ code/test.java
@@ -1,7 +1,28 @@
 import java.util.*;
 public class test{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        while(input.hasNext()){
+            int n=input.nextInt();
+            int m=input.nextInt();
+            int k=input.nextInt();
+            int[] arr=new int[n*m];
+            int count=0;
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    arr[count++]=i*j;
+                }
+            }
+            for(int i=0;i<n*m;i++){
+                System.out.print(arr[i]+" ");
+            }
+            Arrays.sort(arr);
+            System.out.println("");
+            for(int i=0;i<n*m;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println(arr[n*m-k]);
+        }
+    }
 
 }
-
-
-
