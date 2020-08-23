c4fac7069a2a50e35bbd97fb36a0a18c1f1b512a
yuhaishen <2249094924@qq.com>
2020-08-23 12:09:35 +0800

    add DP

---
+++ code/leetcode/DP_509.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class DP_509 {
+    class Solution {
+        public int fib(int N) {
+            int[] dp=new int[N+1];
+            if(N==0){
+                return 0;
+            }
+            else if(N==1){
+                return 1;
+            }
+            dp[0]=0;
+            dp[1]=1;
+            for(int i=2;i<=N;i++){
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+            return dp[N];
+        }
+    }
+}
