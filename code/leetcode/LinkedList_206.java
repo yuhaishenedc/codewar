044b9bb063f9ab6efed7a5074165b3fd10e38c4e
yuhaishen <2249094924@qq.com>
2020-08-25 23:30:18 +0800

    add DP

---
+++ code/leetcode/LinkedList_206.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class LinkedList_206 {
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            if(head==null){
+                return null;
+            }
+            ListNode pre=null;
+            ListNode cur=head;
+            while(cur.next!=null){
+                ListNode temp=cur.next;
+                cur.next=pre;
+                pre=cur;
+                cur=temp;
+            }
+            cur.next=pre;
+            return cur;
+        }
+    }
+}
