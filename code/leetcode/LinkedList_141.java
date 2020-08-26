b99c0583541c07c193e032e8ad1c1c8dab8f41db
yuhaishen <2249094924@qq.com>
2020-08-26 15:44:15 +0800

    add DP

---
+++ code/leetcode/LinkedList_141.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LinkedList_141 {
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            ListNode fast=head;
+            ListNode slow=head;
+            while(fast!=null&&fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+                if(fast==slow){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
