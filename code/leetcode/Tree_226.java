c58b6b95997a0bc06e315bd831aedc7e460eb6e5
yuhaishen <2249094924@qq.com>
2020-08-30 17:08:28 +0800

    add DP

---
+++ code/leetcode/Tree_226.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Tree_226 {
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root==null){
+                return root;
+            }
+            TreeNode temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+            invertTree(root.left);
+            invertTree(root.right);
+            return root;
+        }
+    }
+}
--- code/test.java
+++ code/test.java
@@ -5,6 +5,6 @@ import java.util.Stack;
 public class test {
 
     public static void main(String[] args) {
-        Stack
+        LinkedList
     }
 }
