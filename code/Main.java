1b609e304f93764beef84bad3a58b370e542471e
yuhaishen <2249094924@qq.com>
2020-09-14 07:50:42 +0800

    add DP

--- code/Main.java
+++ code/Main.java
@@ -1,11 +1,45 @@
-import java.util.*;
-import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+import java.util.Scanner;
+public class Main {
 
-public class Main{
-    public static void main(String[] args){
-
-
+    public static void main(String[] args) {
+        int[] arr=new int[]{5,4,3,2,1};
+        helper(arr,0,arr.length-1);
+        System.out.println("result");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 
+    public static void helper(int[] arr,int l,int r){
 
+
+        if(l>=r){
+            return;
+        }
+        int left=l;
+        int right=r-1;
+        while(left<right){
+            while(arr[left]<arr[r]&&left<right){
+                left++;
+            }
+            while(arr[right]>arr[r]&&left<right){
+                right++;
+            }
+            swap(arr,left,right);
+        }
+        if(arr[r]>arr[left]){
+            swap(arr,left,r);
+        }
+        else{
+            left++;
+        }
+        helper(arr,l,left-1);
+        helper(arr,left+1,r);
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
--- code/test.java
+++ code/test.java
@@ -2,11 +2,34 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class test {
     public static void main(String[] args) {
-        ConcurrentHashMap
+        int[] arr=new int[]{-1,4,2,1,9,10};
+        Solution test=new Solution();
+        test.firstMissingPositive(arr);
     }
 }
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0&&nums[i]<n&&nums[nums[i]-1]!=nums[i]){
+                int temp=nums[nums[i]-1];
+                nums[nums[i]-1]=nums[i];
+                nums[i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i-1){
+                return i;
+            }
+        }
+        return n+1;
+    }
+}
+
 
 
