50e32211a2e0834062bd5ae35881a7d417b699b1
yuhaishen <2249094924@qq.com>
2020-09-24 08:38:45 +0800

    add DP

---
+++ code/leetcode/SQL_183.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class SQL_183 {
+    /*
+
+    select Customers.Name as Customers
+    from Customers
+    where Customers.Id not in(
+        select distinct CustomerId
+        from Orders
+    );
+
+     */
+}
