13ebd9f8cbd8f5c3fcdb6f35a4be0c46405690d1
yuhaishen <2249094924@qq.com>
2020-09-24 08:28:09 +0800

    add DP

---
+++ code/leetcode/SQL_620.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class SQL_620 {
+    /*
+
+    select * from cinema
+    where description != 'boring'
+    and id%2=1
+    order by rating desc
+
+     */
+}
--- code/test.java
+++ code/test.java
@@ -1,75 +1,56 @@
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-import java.util.*;
-
 public class test {
-    public static void main(String[] args) throws Exception {
-        int[][] arr=new int[][]{{0,1},{1,1}};
-        Solution test=new Solution();
-        System.out.println(test.maxAreaOfIsland(arr));
-    }
 
+    public static class ThreadPrinter implements Runnable{
+        private String name;
+        private Object prev;
+        private Object self;
 
-
-}
-
-class Solution {
-
-    class node{
-        int x;
-        int y;
-        node(int x,int y){
-            this.x=x;
-            this.y=y;
+        private ThreadPrinter(String name,Object prev,Object self){
+            this.name=name;
+            this.prev=prev;
+            this.self=self;
         }
-    }
 
-    public int maxAreaOfIsland(int[][] grid) {
-        int ret=0;
-        int len1=grid.length;
-        int len2=grid[0].length;
-        for(int i=0;i<len1;i++){
-            for(int j=0;j<len2;j++){
-                if(grid[i][j]==1){
-                    int cnt=1;
-                    grid[i][j]=0;
-                    Queue<node> q=new LinkedList<>();
-                    q.offer(new node(i,j));
-                    while(!q.isEmpty()){
-                        int sc=q.size();
-                        System.out.println(sc);
-                        for(int k=0;k<sc;k++){
-                            node tmp=q.poll();
-                            int x=tmp.x;
-                            int y=tmp.y;
-                            System.out.println("k:"+k+" x:"+x+" y:"+y);
-                            if(x-1>0&&grid[x-1][y]==1){
-                                q.offer(new node(x-1,y));
-                                grid[x-1][y]=0;
-                                cnt++;
-                            }
-                            if(x+1<len1&&grid[x+1][y]==1){
-                                q.offer(new node(x+1,y));
-                                grid[x+1][y]=0;
-                                cnt++;
-                            }
-                            if(y-1>0&&grid[x][y-1]==1){
-                                q.offer(new node(x,y-1));
-                                grid[x][y-1]=0;
-                                cnt++;
-                            }
-                            if(y+1<len2&&grid[x][y+1]==1){
-                                q.offer(new node(x,y+1));
-                                grid[x][y+1]=0;
-                                cnt++;
-                            }
-                        }
+        @Override
+        public void run(){
+            int count=10;
+            while (count>0){
+                synchronized (prev){
+                    synchronized (self){
+                        System.out.println(name);
+                        count--;
+                        self.notifyAll();
                     }
-                    ret=Math.max(ret,cnt);
+                    try {
+                        if(count==0){
+                            prev.notifyAll();
+                        }else {
+                            prev.wait();
+                        }
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
                 }
             }
         }
-        return ret;
     }
+
+    public static void main(String[] args) throws Exception{
+        Object a=new Object();
+        Object b=new Object();
+        Object c=new Object();
+        ThreadPrinter pa=new ThreadPrinter("A",c,a);
+        ThreadPrinter pb=new ThreadPrinter("B",a,b);
+        ThreadPrinter pc=new ThreadPrinter("C",b,c);
+
+        new Thread(pa).start();
+        Thread.sleep(10);
+        new Thread(pb).start();
+        Thread.sleep(10);
+        new Thread(pc).start();
+        Thread.sleep(10);
+
+    }
+
 }
+
