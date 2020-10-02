b12ea2713273bfd4bc01dd09c1ee26ba3ad1665b
yuhaishen <2249094924@qq.com>
2020-10-02 23:12:45 +0800

    add DP

---
+++ code/leetcode/Array_26.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class Array_26 {
+    /*
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cnt=0;
+        int sc=nums.size();
+        if(sc==0){
+            return 0;
+        }
+        int left=0;
+        int right=0;
+        while(right<sc){
+            while(right<sc&&nums[left]==nums[right]){
+                right++;
+            }
+            nums[cnt++]=nums[left];
+            left=right;
+        }
+        return cnt;
+    }
+};
+     */
+}
