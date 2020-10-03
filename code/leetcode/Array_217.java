d16bd2c610440ab9c941b0dd3667d93a03272d0f
yuhaishen <2249094924@qq.com>
2020-10-03 09:29:54 +0800

    add DP

---
+++ code/leetcode/Array_217.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Array_217 {
+    /*
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+        int sc=nums.size();
+        for(int i=0;i<sc;i++){
+            if(s.find(nums[i])!=s.end()){
+                return true;
+            }
+            else{
+                s.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
+     */
+}
