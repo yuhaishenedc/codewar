58ebc0d89662e932e0bcbf1a7fdff3b4b887c75b
yuhaishen <2249094924@qq.com>
2020-10-03 09:17:42 +0800

    add DP

---
+++ code/leetcode/Array_189.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class Array_189 {
+    /*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int sc=nums.size();
+        int count=0;
+        for(int i=0;count<sc;i++){
+            int left=nums[i];
+            int cur=i;
+            do{
+                int next=(cur+k)%sc;
+                int temp=nums[next];
+                nums[next]=left;
+                left=temp;
+                cur=next;
+                count++;
+            }while(cur!=i);
+        }
+    }
+};
+     */
+}
