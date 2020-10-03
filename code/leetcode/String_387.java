eac9d60f94a4d15cada0f8c864498c1a616eb446
yuhaishen <2249094924@qq.com>
2020-10-03 17:01:10 +0800

    add DP

---
+++ code/leetcode/String_387.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class String_387 {
+    /*
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int> m;
+        int sc=s.length();
+        for(int i=0;i<sc;i++){
+            if(m.find(s[i])!=m.end()){
+                m[s[i]]=m[s[i]]+1;
+            }
+            else{
+                m[s[i]]=1;
+            }
+        }
+        for(int i=0;i<sc;i++){
+            if(m[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+     */
+}
