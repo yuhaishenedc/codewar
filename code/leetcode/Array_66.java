634ca383eb7c2eb20c16d4c5eaf8ab5a778f7eef
yuhaishen <2249094924@qq.com>
2020-10-03 14:11:05 +0800

    add DP

---
+++ code/leetcode/Array_66.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class Array_66 {
+    /*
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int sc=digits.size();
+        reverse(digits.begin(),digits.end());
+        int add=1;
+        for(int i=0;i<sc;i++){
+            int tmp=digits[i];
+            digits[i]=(tmp+add)%10;
+            add=(add+tmp)/10;
+        }
+        if(add==1){
+            digits.push_back(1);
+        }
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
+     */
+}
