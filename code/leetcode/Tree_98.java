a736ded1b2fa76d6d77ca491347cf962eb7af5b4
yuhaishen <2249094924@qq.com>
2020-10-04 15:32:30 +0800

    add DP

--- code/leetcode/Tree_98.java
+++ code/leetcode/Tree_98.java
@@ -25,4 +25,30 @@ public class Tree_98 {
 
         }
     }
+    /*
+class Solution {
+public:
+    int prev;
+    bool isValidBST(TreeNode* root) {
+        vector<int> v;
+        helper(root,v);
+        int sc=v.size();
+        for(int i=0;i<sc-1;i++){
+            if(v[i]>=v[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void helper(TreeNode* root,vector<int>& v){
+        if(root!=NULL){
+            helper(root->left,v);
+            v.push_back(root->val);
+            helper(root->right,v);
+        }
+    }
+
+};
+     */
 }
