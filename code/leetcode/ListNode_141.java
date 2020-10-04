c23cc14b4be9dd8b6f0ba24715299c38b7143695
yuhaishen <2249094924@qq.com>
2020-10-04 14:56:04 +0800

    add DP

---
+++ code/leetcode/ListNode_141.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class ListNode_141 {
+    /*
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==NULL){
+                return false;
+            }
+            if(fast->val==slow->val){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+     */
+}
