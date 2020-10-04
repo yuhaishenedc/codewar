5e449dc2eebe9e42f2018da14d2a24fa1804b16f
yuhaishen <2249094924@qq.com>
2020-10-04 14:43:19 +0800

    add DP

---
+++ code/leetcode/ListNode_234.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class ListNode_234 {
+    /*
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<ListNode*> s;
+        ListNode* tmp=head;
+        while(tmp!=NULL){
+            s.push(tmp);
+            tmp=tmp->next;
+        }
+        while(head!=NULL){
+            ListNode* cmp=s.top();
+            s.pop();
+            if(cmp->val!=head->val){
+                return false;
+            }
+            head=head->next;
+        }
+        return true;
+    }
+};
+     */
+}
