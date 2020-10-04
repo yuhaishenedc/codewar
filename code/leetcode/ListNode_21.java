36fcc834f65555087749a5aae61cdd7e23469883
yuhaishen <2249094924@qq.com>
2020-10-04 14:39:02 +0800

    add DP

--- code/leetcode/ListNode_21.java
+++ code/leetcode/ListNode_21.java
@@ -20,4 +20,32 @@ public class ListNode_21 {
             return tmp.next;
         }
     }
+    /*
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head=new ListNode();
+        ListNode* tmp=head;
+        while(l1!=NULL&&l2!=NULL){
+            cout<<"l1:"<<l1->val<<" l2:"<<l2->val<<endl;
+            if(l1->val<l2->val){
+                tmp->next=l1;
+                l1=l1->next;
+            }
+            else{
+                tmp->next=l2;
+                l2=l2->next;
+            }
+            tmp=tmp->next;
+        }
+        if(l1!=NULL){
+            tmp->next=l1;
+        }
+        else{
+            tmp->next=l2;
+        }
+        return head->next;
+    }
+};
+     */
 }
