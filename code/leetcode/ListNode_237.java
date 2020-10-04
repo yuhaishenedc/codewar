649f0fa4a32c65ae02d2800d4b0e6246cfb06c01
yuhaishen <2249094924@qq.com>
2020-10-04 14:09:22 +0800

    add DP

---
+++ code/leetcode/ListNode_237.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class ListNode_237 {
+    /*
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+    }
+};
+     */
+}
