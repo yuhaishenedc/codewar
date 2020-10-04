0d5ad9711d23d386a506497d5f449cecc5d9f492
yuhaishen <2249094924@qq.com>
2020-10-04 09:02:19 +0800

    add DP

---
+++ code/leetcode/String_125.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class String_125 {
+    /*
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> v;
+        string tmp=s;
+        int sc=s.size();
+        for(int i=0;i<sc;i++){
+            char x=tolower(s[i]);
+            if(x-'0'>=0&&x-'0'<=9||x-'a'>=0&&x-'a'<=25){
+                v.push_back(x);
+            }
+        }
+        vector<char> com(v);
+        reverse(v.begin(),v.end());
+        int l=v.size();
+        for(int i=0;i<l;i++){
+            if(v[i]!=com[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+     */
+}
