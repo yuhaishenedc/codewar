fd3f270b157574edcc0487c11f7b54e04594f57f
yuhaishen <2249094924@qq.com>
2020-10-04 09:39:50 +0800

    add DP

---
+++ code/leetcode/String_28.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class String_28 {
+    /*
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1=haystack.length();
+        int len2=needle.length();
+        if(len2==0){
+            return 0;
+        }
+        for(int i=0;i<=len1-len2;i++){
+            if(haystack[i]==needle[0]){
+                bool flag=true;
+                for(int j=0;j<len2;j++){
+                    if(haystack[i+j]!=needle[j]){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
+     */
+}
