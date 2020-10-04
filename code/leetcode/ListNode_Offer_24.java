d21ad1937592f9888579a65c96acee39b0c73bb9
yuhaishen <2249094924@qq.com>
2020-10-04 14:30:05 +0800

    add DP

---
+++ code/leetcode/ListNode_Offer_24.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ListNode_Offer_24 {
+    /*
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        while(head!=NULL){
+            ListNode* tmp=head->next;
+            head->next=prev;
+            prev=head;
+            head=tmp;
+        }
+        return prev;
+    }
+};
+     */
+}
