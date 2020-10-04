66a9b9bcf9d5af9cfdd0bab2cfd3fa40e105fe7e
yuhaishen <2249094924@qq.com>
2020-10-04 14:00:29 +0800

    add DP

--- code/leetcode/LinkedList_Offer_18.java
+++ code/leetcode/LinkedList_Offer_18.java
@@ -18,4 +18,23 @@ public class LinkedList_Offer_18 {
             return head;
         }
     }
+    /*
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        if(head->val==val){
+            return head->next;
+        }
+        ListNode* tmp=head;
+        ListNode* prev=NULL;
+        while(tmp->val!=val){
+            prev=tmp;
+            tmp=tmp->next;
+        }
+        prev->next=tmp->next;
+        return head;
+
+    }
+};
+     */
 }
