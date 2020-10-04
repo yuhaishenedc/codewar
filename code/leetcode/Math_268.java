aa913230ec6092ff365009b0638fbd651eec59b7
yuhaishen <2249094924@qq.com>
2020-10-04 20:22:26 +0800

    add DP

---
+++ code/leetcode/Math_268.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Math_268 {
+    /*
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sc=nums.size();
+        int sum=sc*(sc+1)/2;
+        for(int i=0;i<sc;i++){
+            sum-=nums[i];
+        }
+        return sum;
+    }
+};
+     */
+}
