fd74da818b0f46638be1de9729d4873529dec099
yuhaishen <2249094924@qq.com>
2020-10-16 14:22:37 +0800

    add DP

---
+++ code/leetcode/BitwiseOperation_190.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class BitwiseOperation_190 {
+    class Solution {
+        // you need treat n as an unsigned value
+        public int reverseBits(int n) {
+            int res=0;
+            for(int i=0;i<32;i++){
+                res=(res<<1)|(n&1);
+                n>>=1;
+            }
+            return res;
+        }
+    }
+}
--- code/p.java
+++ code/p.java
@@ -1,5 +1,6 @@
 public class p {
     public static void main(String[] args) {
-
+        int n=-99;
+        System.out.println(n%2);
     }
 }
